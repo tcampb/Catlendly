351a3def929580d670bba47a950beb5426a439cc
Tyler Campbell <tcampb30@gmail.com>
2017-11-18 17:24:15 -0500

    General improvements

--- game_file/catlendly2.py
+++ game_file/catlendly2.py
@@ -38,23 +38,31 @@ class PyMain(object):
         self.screen_rect = self.screen.get_rect()
         self.caption = pygame.display.set_caption("Catlendly")
         self.level = "Intro"
-        self.background = pygame.Surface(self.screen.get_size()).convert()
-        self.game_screen = pygame.Surface((1439, 844)).convert()
+        self.background = pygame.Surface(self.screen.get_size())
+        self.background = self.background.convert()
+        self.background.fill((0, 0, 0))
+        self.game_screen = pygame.Surface((1439, 844))
+        self.game_screen = self.game_screen.convert()
         self.game_screen_rect = self.game_screen.get_rect()
         self.stage_background = load_image("../assets/catlendly_background.png")
         self.lives = 3
         self.health_bar_surface = load_image("../assets/health_bar_100.png")
+        # self.health_bar_surface_rect = self.health_bar_surface.get_rect()
+        # self.health_bar_surface_rect.top = 844
+        # self.health_bar_surface_rect.left = 215
         self.score = 0
 
     def MainLoop(self):
 
-        first_initial = "-"
-        middle_initial = "-"
-        last_initial = "-"
-        full_initials = first_initial + middle_initial + last_initial
+        initial_f = "-"
+        initial_m = "-"
+        initial_l = "-"
+        full_initials = initial_f + initial_m + initial_l
+
         y = 0
         self.time_hit = 0
         self.load_intro_sprites()
+        pygame.display.update()
         #Main game loop
         while self.level != "restart":
             rel_y = y % self.stage_background.get_rect().height
@@ -64,11 +72,11 @@ class PyMain(object):
                 self.screen.blit(self.font_score_two, [575, 650])
                 self.screen.blit(self.font_score_three, [575, 700])
                 self.screen.blit(self.score_cover, PyMain.players_total)
-                self.star_sprites.draw(self.screen) 
+                self.star_sprites.draw(self.screen)
                 pygame.display.update()
                 self.star_sprites.clear(self.screen, self.background)
                 self.star_sprites.empty()
-                self.update_star_sprites()
+                self.update_intro_background()
             elif self.level == "Character_selection":
                 pygame.display.update()
             elif self.level == "Stage_one":
@@ -95,7 +103,7 @@ class PyMain(object):
                     self.ship_sprites.empty()
                     self.Stage_one(True)
                 self.asteroid_sprites.draw(self.screen)
-                self.move_asteroids()
+                self.update_asteroids()
                 self.screen.blit(self.health_bar_surface, [215, 855])
                 self.font_score = load_fonts("Score: %d" % self.score, 20, (255, 255, 255))
                 self.screen.blit(self.font_score, [1180, 870])
@@ -107,13 +115,19 @@ class PyMain(object):
                 if rel_y < 899:
                     self.screen.blit(self.stage_background, (0, rel_y))
             elif self.level == "Game_Over":
-                #Reset initials when user uses backspace
-                pygame.draw.rect(self.screen, (0,0,0), (500, 500, 400, 200))
-                self.font_player_initials = load_fonts("%s%s%s" % (first_initial, middle_initial, last_initial), 90, (255, 255, 255))
+                self.game_over()
+                self.health_bar_surface.fill((0,0,0), Rect((844, 215), (1450, 100)))
+                self.screen.blit(self.reset_initials, [550, 550])
+                self.screen.blit(self.reset_initials, [550, 550])
+                self.font_player_initials = load_fonts("%s%s%s" % (initial_f, initial_m, initial_l), 90, (255, 255, 255))
                 self.screen.blit(self.font_player_initials, [550, 550])
+                self.screen.blit(self.font_gameover, [290, 145])
+                self.screen.blit(self.font_final_score, [450, 300])
+                self.screen.blit(self.font_enter_initials, [350, 400])
                 pygame.display.update()
 
             
+       
             # Event Handler
             for event in pygame.event.get():
                 if event.type == pygame.KEYDOWN and pygame.key.name(event.key) == "escape":
@@ -160,23 +174,23 @@ class PyMain(object):
                     
                         if key == "backspace" or key == "delete":
                             
-                            if last_initial != "-":
-                                last_initial = "-"
-                            elif middle_initial != "-":
-                                middle_initial = "-"
-                            elif first_initial != "-":
-                                first_initial = "-"
+                            if initial_l != "-":
+                                initial_l = "-"
+                            elif initial_m != "-":
+                                initial_m = "-"
+                            elif initial_f != "-":
+                                initial_f = "-"
 
                         if key in string.ascii_letters:
-                            if first_initial == "-":
-                                first_initial = pygame.key.name(event.key).upper()
-                            elif middle_initial == "-":
-                                middle_initial = pygame.key.name(event.key).upper()
-                            elif last_initial == "-":
-                                last_initial = pygame.key.name(event.key).upper()
+                            if initial_f == "-":
+                                initial_f = pygame.key.name(event.key).upper()
+                            elif initial_m == "-":
+                                initial_m = pygame.key.name(event.key).upper()
+                            elif initial_l == "-":
+                                initial_l = pygame.key.name(event.key).upper()
 
                         elif key == "return":
-                            full_initials = "%s%s%s" % (first_initial, middle_initial, last_initial)
+                            full_initials = "%s%s%s" % (initial_f, initial_m, initial_l)
                             PyMain.player_scores[full_initials] = self.score
                             try:
                                 saved_file = open("player_scores.txt", "wb")
@@ -188,8 +202,14 @@ class PyMain(object):
 
                             self.level = "restart"
                         
+
+      
+
+                
         MainWindow = PyMain()
         MainWindow.MainLoop()
+
+
 
     def game_over(self):
         self.asteroid_sprites.clear(self.screen, self.background)
@@ -197,12 +217,10 @@ class PyMain(object):
         self.font_gameover = load_fonts("Game Over", 95, (42, 247, 44))
         self.font_final_score = load_fonts("Score: %d" % self.score, 50, (255, 255, 255))
         self.font_enter_initials = load_fonts("Enter initials:", 50, (255, 255, 255))
-        #Remove healthbar and stage score
-        pygame.draw.rect(self.screen, (0,0,0), (0, 750, 1400, 200))
-        #Add text
-        self.screen.blit(self.font_gameover, [290, 145])
-        self.screen.blit(self.font_final_score, [450, 300])
-        self.screen.blit(self.font_enter_initials, [350, 400])
+        #(270, 91)
+        self.reset_initials = pygame.Surface((300,200))
+        self.reset_initials.fill((0,0,0))
+        self.health_bar_surface.fill((0,0,0))
         
 
     def Character_Selection(self):
@@ -248,10 +266,9 @@ class PyMain(object):
         if reset:
             if self.lives == 0:
                 self.level = "Game_Over"
-                self.game_over()
             else:
                 self.screen.blit(self.font_score, [1180, 870])
-                self.ship_sprites = self.load_ship_sprite(self.character)
+                self.ship_sprites = self.LoadSprites(self.character)
                 self.ship_sprites.draw(self.screen)
                 self.asteroid_sprites.empty()
                 self.stage_time = time.time()
@@ -263,7 +280,7 @@ class PyMain(object):
             self.star_sprites.empty()
             self.stage_time = time.time()
             self.screen.blit(self.health_bar_surface, [844, 215])
-            self.ship_sprites = self.load_ship_sprite(self.character)
+            self.ship_sprites = self.LoadSprites(self.character)
             self.ship_sprites.draw(self.screen)
             self.asteroid_sprites = pygame.sprite.Group()
             
@@ -272,9 +289,11 @@ class PyMain(object):
                 
        
              
-    def load_ship_sprite(self, character):
-        
+    def LoadSprites(self, character):
+        #sprite - base class for visible game objects
         self.ship = Ship(character)
+        #A container class to hold and manage multiple sprite objects
+        #Pass newly created ship object to Group as an arguement
         self.ship_sprites = pygame.sprite.Group(self.ship)
         return self.ship_sprites
 
@@ -309,6 +328,8 @@ class PyMain(object):
             print "Unable to open file."
      
 
+
+
         self.score_cover = pygame.Surface((300, 300))
         self.score_cover.fill((0,0,0))
         font_Catlendly_header = load_fonts("Catlendly", 95, (42, 247, 44))
@@ -321,6 +342,10 @@ class PyMain(object):
         self.font_score_one = load_fonts("%s: %s" % (PyMain.player_names[0], PyMain.player_scores[PyMain.player_names[0]]), 30, (255, 255, 255))
         self.font_score_two = load_fonts("%s: %s" % (PyMain.player_names[1], PyMain.player_scores[PyMain.player_names[1]]), 30, (255, 255, 255))
         self.font_score_three = load_fonts("%s: %s" % (PyMain.player_names[2], PyMain.player_scores[PyMain.player_names[2]]), 30, (255, 255, 255))    
+        # rfont_top_scores = self.screen.blit(font_top_scores, [475, 375])
+        # rfont_score_one = self.screen.blit(font_top_scores, [500, 400])
+        # rfont_score_two = self.screen.blit(font_top_scores, [515, 475])
+        # rfont_score_three = self.screen.blit(font_top_scores, [530, 500])
 
         nNumHorizontal = int(self.width/50)
         nNumVertical = int(self.height/50)
@@ -335,7 +360,7 @@ class PyMain(object):
 
         return rfont_Catlendly_header, rfont_press_to_start
 
-    def update_star_sprites(self):
+    def update_intro_background(self):
         colors = [(0, 0, 255), (255, 255, 255), (255, 0, 0)]
         nNumHorizontal = int(self.width/50)
         nNumVertical = int(self.height/50)
@@ -349,12 +374,13 @@ class PyMain(object):
                 self.star_sprites.add(Stars(pygame.Rect(x, y, 50, 50), random.choice(colors)))
 
     def load_asteroids(self):
-
         asteroid_image_array = ["../assets/asteroid1.png", "../assets/asteroid2.png", "../assets/asteroid3.png", "../assets/asteroid4.png"]
+        
         if int(self.stage_time + 30) < int(time.time()):
             if random.random() < 0.3:
                 self.asteroid_sprites.add(Asteroid(random.choice(asteroid_image_array),
                                                    random.randint(20, self.width)))
+
         elif int(self.stage_time + 15) < int(time.time()):
             if random.random() < 0.2:
                 self.asteroid_sprites.add(Asteroid(random.choice(asteroid_image_array),
@@ -363,18 +389,25 @@ class PyMain(object):
             if random.random() < 0.1:
                 self.asteroid_sprites.add(Asteroid(random.choice(asteroid_image_array),
                                                    random.randint(20, self.width)))
-    def move_asteroids(self):
+        
+          
+      
+    def update_asteroids(self):
+        for sprite in self.asteroid_sprites.sprites():
+            sprite.move(self.screen_rect)
+            self.asteroid_sprites.add(sprite)
 
-        for asteroid in self.asteroid_sprites.sprites():
-            asteroid.move(self.screen_rect)
-            self.asteroid_sprites.add(asteroid)
+
 
 
 class Ship(pygame.sprite.Sprite):
 
     def __init__(self, character):
+        #Create sprite object
         pygame.sprite.Sprite.__init__(self)
+        #Surface is the pygame object for representing images 
         self.image = load_image(character)
+        #pygame.Rect - object for storting rectangular coordinates 
         self.rect = self.image.get_rect()
         self.rect.top = 770
         self.rect.left = 685
@@ -385,6 +418,8 @@ class Ship(pygame.sprite.Sprite):
     def move(self, key, screen_rect):
         xMove = 0
         yMove = 0
+
+
         if (key == pygame.K_RIGHT):
             xMove = self.x_dist
         elif (key == pygame.K_LEFT):
@@ -393,8 +428,10 @@ class Ship(pygame.sprite.Sprite):
             yMove = self.y_dist
         elif (key == pygame.K_UP):
             yMove = -self.y_dist
+
         self.rect.move_ip(xMove, yMove)
         self.rect.clamp_ip(screen_rect)
+        
         
 class Asteroid(pygame.sprite.Sprite):
 
@@ -405,6 +442,7 @@ class Asteroid(pygame.sprite.Sprite):
         self.rect = Rect(x_axis, 0, 35, 32)
 
     def move(self, screen_rect):
+
         self.rect.move_ip(0, 10)
         
 
